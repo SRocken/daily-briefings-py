a09cf84c0fa79582e4c10ac39f093ec4132f0585
SRocken <65569622+SRocken@users.noreply.github.com>
2020-06-18 21:01:15 -0400

    Added weather functionality

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,11 +1,12 @@
 # web_app/__init__.py
 
 import os
+
 from dotenv import load_dotenv
 from flask import Flask
 
 from web_app.routes.home_routes import home_routes
-#from web_app.routes.weather_routes import weather_routes
+from web_app.routes.weather_routes import weather_routes
 
 load_dotenv()
 
@@ -16,7 +17,7 @@ def create_app():
     app.config["SECRET_KEY"] = SECRET_KEY
 
     app.register_blueprint(home_routes)
-    #app.register_blueprint(weather_routes)
+    app.register_blueprint(weather_routes)
 
     return app
 
